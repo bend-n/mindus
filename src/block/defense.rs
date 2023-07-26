efbfe9c95d672efb3d3c33c65fd527e78e4ffa6c
bendn <bend.n@outlook.com>
2023-07-26 12:17:57 +0700

    overdrive

--- src/block/defense.rs
+++ src/block/defense.rs
@@ -2,12 +2,12 @@
 use crate::block::simple::{cost, make_simple};
 use crate::block::*;
 make_simple!(DefenseBlock);
-make_simple!(MenderBlock => |_, _, _, buff: &mut DataRead| read_mender(buff));
+make_simple!(HeatedBlock => |_, _, _, buff: &mut DataRead| read_heated(buff));
 make_register! {
-    "mender" => MenderBlock::new(1, true, cost!(Copper: 25, Lead: 30));
-    "mend-projector" => MenderBlock::new(2, true, cost!(Copper: 50, Lead: 100, Titanium: 25, Silicon: 40));
-    "overdrive-projector" => DefenseBlock::new(2, true, cost!(Lead: 100, Titanium: 75, Silicon: 75, Plastanium: 30));
-    "overdrive-dome" => DefenseBlock::new(3, true, cost!(Lead: 200, Titanium: 130, Silicon: 130, Plastanium: 80, SurgeAlloy: 120));
+    "mender" => HeatedBlock::new(1, true, cost!(Copper: 25, Lead: 30));
+    "mend-projector" => HeatedBlock::new(2, true, cost!(Copper: 50, Lead: 100, Titanium: 25, Silicon: 40));
+    "overdrive-projector" => HeatedBlock::new(2, true, cost!(Lead: 100, Titanium: 75, Silicon: 75, Plastanium: 30));
+    "overdrive-dome" => HeatedBlock::new(3, true, cost!(Lead: 200, Titanium: 130, Silicon: 130, Plastanium: 80, SurgeAlloy: 120));
     "force-projector" => DefenseBlock::new(3, true, cost!(Lead: 100, Titanium: 75, Silicon: 125));
     "regen-projector" => DefenseBlock::new(3, true, cost!(Silicon: 80, Tungsten: 60, Oxide: 40, Beryllium: 80));
     "shock-mine" => DefenseBlock::new(1, true, cost!(Lead: 25, Silicon: 12));
@@ -23,6 +23,6 @@ make_register! {
 /// format:
 /// - heat: [`f32`]
 /// - phase heat: [`f32`]
-fn read_mender(buff: &mut DataRead) -> Result<(), DataReadError> {
+fn read_heated(buff: &mut DataRead) -> Result<(), DataReadError> {
     buff.skip(8)
 }
