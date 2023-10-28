ecb3d4b989648fb2fa053597b683617e1ffc0ae9
bendn <bend.n@outlook.com>
2023-10-29 06:08:28 +0700

    fix scaling

--- mindus/src/block/logic.rs
+++ mindus/src/block/logic.rs
@@ -138,14 +138,10 @@ impl BlockLogic for CanvasBlock {
                 (*r, *g, *b) = PALETTE[y as usize];
                 *a = 255;
             }
-            let img = unsafe {
-                fimg::scale::Nearest::scale(
-                    img.as_ref(),
-                    (s * self.size as u32) - offset * 2,
-                    (s * self.size as u32) - offset * 2,
-                )
-                .boxed()
-            };
+            let img = img.scale::<fimg::scale::Nearest>(
+                (s * self.size as u32) - offset * 2,
+                (s * self.size as u32) - offset * 2,
+            );
             let mut borders = load!("canvas", s);
             unsafe { borders.overlay_at(&ImageHolder::from(img), offset, offset) };
             return borders;
